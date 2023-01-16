fbecc8bdc8d77dc7386a6fdb9f202c5fee86130d
emerald2424 <109916178+emerald2424@users.noreply.github.com>
2023-01-16 17:43:17 +0200

    pagination added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
+import './css/common.css'
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import "notiflix/dist/notiflix-3.2.6.min.css";
 // import axios from "axios";
 
 const searchForm = document.querySelector('#search-form');
 const input = document.querySelector('input[name="searchQuery"]')
-const btn = document.querySelector('btn[type="submit"]')
-
+const loadMoreBtn = document.querySelector('.load-more');
+let page = 1;
+let query = '';
 
 const gallery = document.querySelector('.gallery');
 const API_KEY = '32852633-0625716e22432e941df8357a0';
@@ -15,22 +17,22 @@ searchForm.addEventListener('submit', onSearch);
 function onSearch(evt) {
     evt.preventDefault();
     gallery.innerHTML = '';
-    const query = input.value;
+    query = input.value;
     
-    fetchImages(query)
+    fetchImages(query, page = 1)
     .then(data => {
         if (data.hits.length === 0) {
             return Notify.failure("Sorry, there are no images matching your search query. Please try again.");
         }
-        console.log(searchForm)
-        console.log(query)
         Notify.success(`Hooray! We found ${data.totalHits} images.`);
-        renderGallery(data.hits)})
+        renderGallery(data.hits)
+        loadMoreBtn.hidden = false;
+    })
     .catch(err => console.log(err))
 }
 
-function fetchImages(query) {
-    return fetch(`https://pixabay.com/api/?key=${API_KEY}&image_type=photo&orientation=horizontal&safesearch=true&q=${query}`)
+function fetchImages(query, page) {
+    return fetch(`https://pixabay.com/api/?key=${API_KEY}&image_type=photo&orientation=horizontal&safesearch=true&q=${query}&page=${page}&per_page=40`)
     .then(resp => {
         if (!resp.ok) {
             throw new Error(resp.statusText);
@@ -42,24 +44,48 @@ function fetchImages(query) {
 function renderGallery(images) {
     
     const markup = images.map(image => `
+    <a href="${image.largeImageURL}">
         <div class="photo-card">
-            <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+            <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" width="280px" height="140"/>
             <div class="info">
                 <p class="info-item">
-                    <b>Likes</b>${image.likes}
+                    <b>Likes</b><br>${image.likes}
                 </p>
                 <p class="info-item">
-                    <b>Views</b>${image.views}
+                    <b>Views</b><br>${image.views}
                 </p>
                 <p class="info-item">
-                    <b>Comments</b>${image.comments}
+                    <b>Comments</b><br>${image.comments}
                 </p>
                 <p class="info-item">
-                    <b>Downloads</b>${image.downloads}
+                    <b>Downloads</b><br>${image.downloads}
                 </p>
-        </div>`
+            </div>    
+        </div>
+    </a>
+        `
     ).join('')
 
     gallery.insertAdjacentHTML('beforeend', markup);
-    
 }
+
+loadMoreBtn.addEventListener('click', loadMore)
+
+function loadMore() {
+    page += 1;
+    
+    fetchImages(query, page)
+    .then(data => {
+        renderGallery(data.hits)
+        console.log("Page " + page)
+        
+        
+        const pages = data.totalHits / 40;
+        // console.log("Pages ", pages)
+        if (pages < page) {
+            loadMoreBtn.hidden = true;
+        }
+    })
+    .catch(err => console.log(err))
+}
+
